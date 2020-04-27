a966cc2c5f422f7898641fedb841279051000e31
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-27 07:08:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,8 @@ class Anagram
   
   def match(word_array)
     word_array.select.do |word|
-      word.split("").sort == @word.split
+      word.split("").sort == @word.split("").split
+    end 
   end 
   
 end 
