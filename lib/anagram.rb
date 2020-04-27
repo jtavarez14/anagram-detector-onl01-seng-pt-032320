df07612725896567782ae1324c747853a1980d72
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-27 07:10:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,14 @@
-# Your code goes here!
 class Anagram
+
   attr_accessor :word
-  
+
   def initialize(word)
-    @word = word 
-  end 
-  
+    @word = word
+  end
+
   def match(word_array)
     word_array.select do |word|
-      word.split("").sort == @word.split("").split
-    end 
-  end 
-
-end  
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
